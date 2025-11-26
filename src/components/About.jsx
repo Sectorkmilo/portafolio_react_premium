b7f07c748e522e77acdbe371cadebe578ec34e47
Sectorkmilo <camilo892009@gmail.com>
2025-11-25 23:33:40 -0500

    se cambia el texto sobre mi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,11 @@ export default function About(){
   return (
     <section id="about" className="py-16 bg-white text-black">
       <div className="max-w-6xl mx-auto px-6">
-        <h2 className="text-3xl font-bold mb-4">Sobre Nosotros</h2>
+        <h2 className="text-3xl font-bold mb-4">Sobre mi</h2>
         <div className="md:flex gap-8 items-center">
           <div className="md:w-1/2 glass p-6 rounded-xl">
             <p className="text-gray-800">
-              Somos un equipo multidisciplinario con experiencia en proyectos sociales, educativos y tecnológicos. Nos enfocamos en el impacto, la escalabilidad y la calidad del trabajo.
+              Este trabajo se desarrolla para el curso de Mantenimiento de Software con el profesor Jose Ruiz, sobre los comandos para el control de versiones con GitHub.
             </p>
           </div>
           <div className="md:w-1/2 mt-6 md:mt-0">
